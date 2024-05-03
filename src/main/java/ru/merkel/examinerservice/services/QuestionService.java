2321d96b8eaa7988ae525ed49781b90c1e77d2c8
Mariia <mariia_merkel@mail.ru>
2024-05-03 13:44:23 +0300

    Написала интерфейс для работы с вопросами

---
+++ src/main/java/ru/merkel/examinerservice/services/QuestionService.java
@@ -0,0 +1,18 @@
+package ru.merkel.examinerservice.services;
+
+import ru.merkel.examinerservice.models.Question;
+
+import java.util.Collection;
+
+public interface QuestionService {
+    Question add(String question, String answer);
+
+    Question add(Question question);
+
+    Question remove(Question question);
+
+    Collection<Question> getAll();
+
+    Question getRandomQuestion();
+
+}
