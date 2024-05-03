36d7389ec41f7addbfe4aef4072d61902a4a1cf7
Mariia <mariia_merkel@mail.ru>
2024-05-03 12:38:59 +0300

    initional commit

---
+++ src/test/java/ru/merkel/examinerservice/ExaminerserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.merkel.examinerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExaminerserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
