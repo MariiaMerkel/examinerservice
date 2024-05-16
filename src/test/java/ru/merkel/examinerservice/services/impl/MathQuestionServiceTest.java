fe27d8c10f6fefcf9d4556ee4b26b988d2817961
MariiaMerkel <mariia_merkel@mail.ru>
2024-05-16 10:53:36 +0300

    Покрыла тестами MathQuestionService с моками

--- src/test/java/ru/merkel/examinerservice/services/impl/MathQuestionServiceTest.java
+++ src/test/java/ru/merkel/examinerservice/services/impl/MathQuestionServiceTest.java
@@ -3,6 +3,10 @@ package ru.merkel.examinerservice.services.impl;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import ru.merkel.examinerservice.exceptions.QuestionAlreadyAddedException;
 import ru.merkel.examinerservice.exceptions.QuestionNotFoundException;
 import ru.merkel.examinerservice.models.Question;
@@ -10,26 +14,28 @@ import ru.merkel.examinerservice.repositiories.impl.MathQuestionRepository;
 import ru.merkel.examinerservice.services.QuestionService;
 
 import java.util.Collection;
+import java.util.HashSet;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasItems;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 import static ru.merkel.examinerservice.constants.MathQuestionsConstants.*;
 
+@ExtendWith(MockitoExtension.class)
 class MathQuestionServiceTest {
 
-    public QuestionService mathQuestionService = new MathQuestionService(new MathQuestionRepository());
+    @Mock
+    public MathQuestionRepository mathQuestionRepository;
 
-    @BeforeEach
-    public void setUp() {
-        mathQuestionService.add(MATH_QUESTION_1, MATH_ANSWER_1);
-        mathQuestionService.add(MATH_QUESTION_2, MATH_ANSWER_2);
-        mathQuestionService.add(MATH_QUESTION_3, MATH_ANSWER_3);
-    }
+    @InjectMocks
+    public MathQuestionService mathQuestionService;
 
     @Test
     void shouldReturnAddedQuestion() {
+        when(mathQuestionRepository.add(MATH_OBJECT_QUESTION_4)).thenReturn(MATH_OBJECT_QUESTION_4);
+        when(mathQuestionRepository.add(MATH_OBJECT_QUESTION_5)).thenReturn(MATH_OBJECT_QUESTION_5);
         Question actual = mathQuestionService.add(MATH_QUESTION_4, MATH_ANSWER_4);
         Question actual2 = mathQuestionService.add(MATH_OBJECT_QUESTION_5);
         assertEquals(MATH_OBJECT_QUESTION_4, actual);
@@ -38,31 +44,37 @@ class MathQuestionServiceTest {
 
     @Test
     void shouldReturnAlreadyAddedException() {
+        when(mathQuestionRepository.add(MATH_OBJECT_QUESTION_1)).thenThrow(QuestionAlreadyAddedException.class);
+        when(mathQuestionRepository.add(MATH_OBJECT_QUESTION_2)).thenThrow(QuestionAlreadyAddedException.class);
         assertThrows(QuestionAlreadyAddedException.class, () -> mathQuestionService.add(MATH_QUESTION_1, MATH_ANSWER_1));
         assertThrows(QuestionAlreadyAddedException.class, () -> mathQuestionService.add(MATH_OBJECT_QUESTION_2));
     }
 
     @Test
     void shouldReturnRandomQuestion() {
+        when(mathQuestionRepository.getAll()).thenReturn(MATH_QUESTIONS);
         Question actual = mathQuestionService.getRandomQuestion();
         assertThat(MATH_QUESTIONS, hasItems(actual));
     }
 
     @Test
     void shouldThrowNotFoundException() {
-        mathQuestionService.removeAll();
+        when(mathQuestionRepository.getAll()).thenReturn(new HashSet<>());
         assertThrows(QuestionNotFoundException.class, () -> mathQuestionService.getRandomQuestion());
     }
 
     @Test
     void shouldReturnRemovedQuestion() {
+        when(mathQuestionRepository.remove(MATH_OBJECT_QUESTION_1)).thenReturn(MATH_OBJECT_QUESTION_1);
+        when(mathQuestionRepository.remove(MATH_OBJECT_QUESTION_2)).thenThrow(QuestionNotFoundException.class);
         Question actual = mathQuestionService.remove(MATH_OBJECT_QUESTION_1);
         assertEquals(MATH_OBJECT_QUESTION_1, actual);
-        assertThrows(QuestionNotFoundException.class, () -> mathQuestionService.remove(MATH_OBJECT_QUESTION_1));
+        assertThrows(QuestionNotFoundException.class, () -> mathQuestionService.remove(MATH_OBJECT_QUESTION_2));
     }
 
     @Test
     void shouldReturnAlQuestions() {
+        when(mathQuestionRepository.getAll()).thenReturn(MATH_QUESTIONS);
         Collection<Question> actual = mathQuestionService.getAll();
         assertThat(actual, Matchers.containsInAnyOrder(
                 MATH_OBJECT_QUESTION_1,
